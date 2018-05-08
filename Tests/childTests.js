11929a58fbdc2b67b4eb9c4d1bd192080d2d4b4b
Seth Childers <sethchilders92@gmail.com>
2018-05-08 13:21:57 -0600

    Added testing, though it won't work because the child module needs to be run with Disperse Welcome Folder, so teting is not possible at this time.

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,13 +3,28 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
+    tap.test('add-course-maintenance-log', (test) => {
+        /* Get the Instructor Resources module */
+    canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules?search_term=Instructor Resources`, (err, currModule) => {
+        if (err) {
+            course.error(new Error(err));
+            test.end();
+            callback(null);
+            return;
+        }
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules/${currModule[0].id}/items?search_term=Course Maintenance Log`, (err, moduleItem) => {
+            if (err) {
+                course.error(new Error(err));
+                test.end();
+                callback(null);
+                return;
+            }
+            if (moduleItem.length !== 0) {
+                test.equal(`Course Maintenance Log`, moduleItem.title);
+            } 
+            test.end();
+    });
 
-        test.pass('potato');
-        test.pass('tomato');
-        test.fail('avacado');
-
-        test.end();
     });
 
     // Always call the callback in your childTests with just null
